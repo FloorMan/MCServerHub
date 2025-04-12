838b6d8735cc40d459684cfaa27249fe898d7cfc
FloorMan <diego0610018657@gmail.com>
2025-04-12 15:45:09 -0400

    Updated Server.js and added .env files

--- MCserver/server.js
+++ MCserver/server.js
@@ -1,4 +1,9 @@
 const express = require('express'); // Import express
+require('dotenv').config(); // Load environment variables from .env file
+const path = require('path'); // Import path module to handle file paths
+const fs = require('fs'); // Import file system module to handle file operations
+const { exec } = require('child_process'); // Import child_process module to execute shell commands
+const { spawn } = require('child_process'); // Import child_process module to spawn new processes
 const app = express();              // Create an instance of express
 const cors = require('cors');          // Import cors for cross-origin resource sharing
 const corsOptions = {
@@ -10,8 +15,21 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Use cors middleware to allow cross-origin requests
 //app.use(express.json()); // Middleware to parse JSON requests
 
+const LOCAL_IP = process.env.DIGITAL_OCEAN_IP; // Get the local IP address from environment variables
+const MINECRAFT_SERVER = process.env.MINECRAFT_SERVER; // Get the Minecraft server address from environment variables
+const PORT = process.env.PORT || 8080; // Set the port to the value from environment variables or default to 8080
 
 
+app.use(express.static(path.join(__dirname, '../MC\ Server\ Hub'))) // Serve the static files from the 'MC Server HUB' directory
+
+app.get("/", (req, res) => { // Define a GET route for the root URL
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+    res.sendFile(path.join(__dirname, "../MC\ Server\ Hub/index.html")); // Serve the index.html file
+});
 
 
 app.get("/api", (req, res) => { // Define a GET route
@@ -30,7 +48,7 @@ app.get("/api/ip", (req, res) => {
     res.setHeader("Access-Control-Max-Age", "1800");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
-    res.json({ ip: "22.ip.gl.ply.gg:44710" }); // Key-value pair in JSON format
+    res.json({ ip: MINECRAFT_SERVER }); // Key-value pair in JSON format
 })
 
 // Start the server on port 8080
